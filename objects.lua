4b974ebcb44ec556d02d3adbeee8863a386522a1
Edgar <ediaz-gutierrez@valcom.com>
2024-12-08 01:40:46 -0500

    backing up all present files for Sagewood Map

---
+++ objects.lua
@@ -0,0 +1,8 @@
+objects = {
+  { name = "", type = "SpawnPoint", x = 5771, y = 10140, z = 0, width = 1, height = 1, properties = { Professions = "all" } },
+  { name = "", type = "SpawnPoint", x = 5640, y = 10384, z = 0, width = 1, height = 1, properties = { Professions = "all" } },
+  { name = "", type = "SpawnPoint", x = 5567, y = 10257, z = 0, width = 1, height = 1, properties = { Professions = "all" } },
+  { name = "", type = "SpawnPoint", x = 5852, y = 10374, z = 0, width = 1, height = 1, properties = { Professions = "all" } },
+  { name = "", type = "SpawnPoint", x = 5880, y = 10479, z = 0, width = 1, height = 1, properties = { Professions = "all" } },
+  { name = "", type = "SpawnPoint", x = 5946, y = 10332, z = 2, width = 1, height = 1, properties = { Professions = "all" } }
+}
---
+++ spawnpoints.lua
@@ -0,0 +1,52 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 19, worldY = 33, posX = 71, posY = 240, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 240, posY = 184, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 167, posY = 57, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 152, posY = 174, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 180, posY = 279, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 246, posY = 132, posZ = 2 }
+  },
+  fireofficer = {
+    { worldX = 19, worldY = 33, posX = 71, posY = 240, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 240, posY = 184, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 167, posY = 57, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 152, posY = 174, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 180, posY = 279, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 246, posY = 132, posZ = 2 }
+  },
+  parkranger = {
+    { worldX = 19, worldY = 33, posX = 71, posY = 240, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 240, posY = 184, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 167, posY = 57, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 152, posY = 174, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 180, posY = 279, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 246, posY = 132, posZ = 2 }
+  },
+  policeofficer = {
+    { worldX = 19, worldY = 33, posX = 71, posY = 240, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 240, posY = 184, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 167, posY = 57, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 152, posY = 174, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 180, posY = 279, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 246, posY = 132, posZ = 2 }
+  },
+  securityguard = {
+    { worldX = 19, worldY = 33, posX = 71, posY = 240, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 240, posY = 184, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 167, posY = 57, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 152, posY = 174, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 180, posY = 279, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 246, posY = 132, posZ = 2 }
+  },
+  unemployed = {
+    { worldX = 19, worldY = 33, posX = 71, posY = 240, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 240, posY = 184, posZ = 0 },
+    { worldX = 18, worldY = 34, posX = 167, posY = 57, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 152, posY = 174, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 180, posY = 279, posZ = 0 },
+    { worldX = 19, worldY = 34, posX = 246, posY = 132, posZ = 2 }
+  }
+}
+end
